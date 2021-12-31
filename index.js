e554879228fe7636835dc21bf38867d977d1d37e
Andre Oliveira <69281348+AndreLPO@users.noreply.github.com>
2021-12-31 09:33:13 -0300

    Projeto inicial do curso de GraphQL da Udemy

---
+++ index.js
@@ -0,0 +1,119 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const usuarios = [
+  {
+    id: 1,
+    nome: "Andre",
+    email: "andre@email.com",
+    idade: 21,
+  },
+  {
+    id: 2,
+    nome: "Evelin",
+    email: "evelin@email.com",
+    idade: 22,
+  },
+  {
+    id: 3,
+    nome: "Juninho",
+    email: "juninho@email.com",
+    idade: 17,
+  },
+];
+
+// Schema
+const typeDefs = gql`
+  scalar Date
+
+  type Usuario {
+    id: ID
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+  }
+
+  type Produto {
+    nome: String!
+    preco: Float!
+    desconto: Float
+    precoComDesconto: Float
+  }
+
+  # Pontos de entrada da sua API!
+  # Consulta:
+  type Query {
+    ola: String
+    horaAtual: Date
+    usuarioLogado: Usuario
+    produtoEmDestaque: Produto
+    numerosMegaSena: [Int!]!
+    usuarios: [Usuario!]!
+    usuario(id: ID): Usuario
+  }
+`;
+
+const resolvers = {
+  Usuario: {
+    salario(usuario) {
+      return usuario.salario_real;
+    },
+  },
+
+  Produto: {
+    precoComDesconto(produto) {
+      return produto.preco * (1 - produto.desconto);
+    },
+  },
+
+  // Consulta dos dados, pode consumir de qualquer lugar (Memória, API, BD, etc...)
+  Query: {
+    ola() {
+      return "Basta retornar uma string";
+    },
+    horaAtual() {
+      return new Date();
+    },
+    usuarioLogado() {
+      return {
+        id: 1,
+        nome: "André",
+        email: "email@email.com",
+        idade: 23,
+        salario_real: 2500.0,
+        vip: false,
+      };
+    },
+    produtoEmDestaque() {
+      return {
+        nome: "Arroz",
+        preco: 25.0,
+        desconto: 0.5,
+      };
+    },
+    numerosMegaSena() {
+      const crescente = (a, b) => a - b;
+      return Array(6)
+        .fill(0)
+        .map(() => parseInt(Math.random() * 60 + 1))
+        .sort(crescente);
+    },
+    usuarios() {
+      return usuarios;
+    },
+    usuario(_, { id }) {
+      const selecionados = usuarios.filter((u) => u.id == id);
+      return selecionados ? selecionados[0] : null;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Executando em ${url}`);
+});
